06872440a7c9c530c68cfdfcaf826eecd2dcd05a
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-19 12:32:15 +0500

    feat: add user routes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
 });
---
+++ src/routes/user.routes.js
@@ -0,0 +1,41 @@
+import { express } from "express";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
+import { randomBytes, createHmac  } from "crypto";
+
+// Database Import
+import db from "./../config/db/index.js";
+
+// Model Import
+import { usersTable } from "./../models/index.js";
+
+const router = express.Router();
+
+router.post("/signup", (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  // check existing user
+  const [existingUser] = await db
+    .select({ id: usersTable.id })
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+
+
+  if (existingUser) {
+    return res.status(400).json({ error: `User with email ${email} already exists` });
+  }
+
+  // hash password for new user
+  const salt = randomBytes(256).toString("hex");
+  const hashedPassword = createHmac("sha256", salt).update(password).digest("hex");
+
+  // create new user
+  const [newUser] = await db
+    .insert(usersTable)
+    .values({ firstname, lastname, email, password: hashedPassword, salt })
+    .returning({ id: usersTable.id });
+
+  res.status(201).json({ data :{userId: newUser.id} });
+});
+
+export default router;
