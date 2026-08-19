d687fe823246081d5b1dee47fce2c33f13d87fa5
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-19 12:49:51 +0500

    feat: add user validation

---
+++ src/validation/request.validation.js
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const signUpRequestSchema = z.object({
+  firstname: z.string(),
+  lastname: z.string().optional(),
+  email: z.string().email(),
+  password: z.string().min(6).max(12),
+});
