be317654b234d792f494f92a027ead46e55e11a2
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-21 17:57:49 +0500

    fix: added user login route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { eq } from "drizzle-orm";
+import jwt from "jsonwebtoken";
 
 // Utils
 import { hashPasswordWithSalt } from "./../utils/hash.js";
 
 // Request Validation
-import { signUpRequestSchema } from "./../validation/request.validation.js";
+import {
+  signUpRequestSchema,
+  loginRequestSchema,
+} from "./../validation/request.validation.js";
 
 // User Service
 import {
@@ -23,8 +26,10 @@ export const userRouter = express.Router();
 userRouter.post("/signup", async (req, res) => {
   try {
     // Validate request body
-    const validationResult = await signUpRequestSchema.parseAsync(req.body);
-    const { firstname, lastname, email, password } = validationResult;
+    const validationSignUpRequest = await signUpRequestSchema.parseAsync(
+      req.body,
+    );
+    const { firstname, lastname, email, password } = validationSignUpRequest;
 
     // Check existing user
     const existingUser = await getUserByEmail(email);
@@ -48,6 +53,45 @@ userRouter.post("/signup", async (req, res) => {
       salt,
     );
     res.status(201).json({ data: { userId: newUser.id } });
+  } catch (error) {
+    res.status(400).json({
+      error: error instanceof Error ? error.message : "Invalid request",
+    });
+  }
+});
+
+// Handle user login
+userRouter.post("/login", async (req, res) => {
+  try {
+    const validationLoginRequest = await loginRequestSchema.parseAsync(
+      req.body,
+    );
+
+    if (validationLoginRequest.error) {
+      return res.status(400).json({ error: validationLoginRequest.error });
+    }
+
+    const { email, password } = validationLoginRequest;
+
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: `User with email ${email} does not exist` });
+    }
+
+    const { hashedPassword } = hashPasswordWithSalt(password, user.salt);
+
+    if (hashedPassword !== user.password) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "5m",
+    });
+    res.status(200).json({ data: { token } });
   } catch (error) {
     res.status(400).json({
       error: error instanceof Error ? error.message : "Invalid request",
